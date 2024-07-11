ffc3407a44b1e8c8511f8e3b8378b55007acde65
boduroglu <burakboduroglu0@gmail.com>
2024-07-11 13:58:43 +0300

    Sign Up Page Design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,56 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
+import Colors from "@/constants/Colors";
+import {TouchableOpacity} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 export {
-  ErrorBoundary,
+    ErrorBoundary,
 } from 'expo-router';
 
 SplashScreen.preventAutoHideAsync();
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+const InitialLayout = () => {
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{headerShown: false}}/>
+            <Stack.Screen name="signup" options={{
+                title: "",
+                headerBackTitle: '',
+                headerShadowVisible: false,
+                headerStyle: {backgroundColor: Colors.background},
+                headerLeft: () => (
+                    <TouchableOpacity>
+                        <Ionicons name="arrow-back" size={32} color={Colors.dark}/>
+                    </TouchableOpacity>
+                )
+            }}/>
+        </Stack>
+    )
 }
 
-function RootLayoutNav() {
-  return (
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown: false}} />
-      </Stack>
-  );
+const RootLayoutNav = () => {
+    return <InitialLayout/>
 }
+
+export default RootLayoutNav;
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,11 +1,82 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform} from 'react-native';
+import {defaultStyles} from '../constants/Styles'
+import Colors from "@/constants/Colors";
+import {useState} from 'react';
+import {Link} from "expo-router";
 
 const Page = () => {
+    const [countryCode, setCountryCode] = useState('+90')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const keyboardVerticalOffset = Platform.OS === 'ios' ? 50 : 0
+
+    const onSignup = () => {
+    }
+
     return (
-        <View>
-            <Text>Page</Text>
-        </View>
+        <KeyboardAvoidingView style={{flex:1}} behavior="padding" keyboardVerticalOffset={keyboardVerticalOffset}>
+            <View style={defaultStyles.container}>
+                <Text style={defaultStyles.header}>Let's get started!</Text>
+                <Text style={defaultStyles.descriptionText}>
+                    Enter your phone number. We will send you a confirmation code there.
+                </Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Country code"
+                        placeholderTextColor={Colors.gray}
+                        value={countryCode}
+                        onChangeText={setCountryCode}
+                        keyboardType="phone-pad"
+                    />
+                    <TextInput
+                        style={[styles.input, {flex: 1}]}
+                        placeholder="Mobile number"
+                        placeholderTextColor={Colors.gray}
+                        keyboardType="phone-pad"
+                        value={phoneNumber}
+                        onChangeText={setPhoneNumber}
+                    />
+                </View>
+                <Link href={'/login'} replace asChild>
+                    <TouchableOpacity>
+                        <Text style={defaultStyles.textLink}>Already have an account?</Text>
+                    </TouchableOpacity>
+                </Link>
+                <View style={{flex: 1}}/>
+                <TouchableOpacity
+                    style={[
+                        defaultStyles.pillButton,
+                        phoneNumber !== '' ? styles.enabled : styles.disabled,
+                        { marginBottom: 30}
+                    ]}
+                    onPress={onSignup}>
+                    <Text style={defaultStyles.buttonText}>
+                        Sign Up
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
 export default Page
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginVertical: 30,
+        flexDirection: 'row'
+    },
+    input: {
+        backgroundColor: Colors.lightGray,
+        padding: 20,
+        borderRadius: 16,
+        fontSize: 16,
+        marginRight: 10
+    },
+    enabled: {
+        backgroundColor: Colors.primary
+    },
+    disabled: {
+        backgroundColor: Colors.primaryMuted
+    }
+});
